f0bab81521a40fe2a2e666101371a510984d8eba
Jordi <jordi.blanco.corrales@gmail.com>
2014-09-05 11:52:18 +0200

    Primera Version de

---
+++ Leer_USB/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmPen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.PensComboBox = New System.Windows.Forms.ComboBox
+        Me.ExitButton = New System.Windows.Forms.Button
+        Me.TimerPenDrives = New System.Windows.Forms.Timer(Me.components)
+        Me.ButtonPen = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'PensComboBox
+        '
+        Me.PensComboBox.FormattingEnabled = True
+        Me.PensComboBox.Location = New System.Drawing.Point(39, 28)
+        Me.PensComboBox.Name = "PensComboBox"
+        Me.PensComboBox.Size = New System.Drawing.Size(209, 21)
+        Me.PensComboBox.TabIndex = 0
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(148, 99)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(60, 27)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "end"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'TimerPenDrives
+        '
+        Me.TimerPenDrives.Interval = 1000
+        '
+        'ButtonPen
+        '
+        Me.ButtonPen.Image = Global.WindowsApplication1.My.Resources.Resources.PEN
+        Me.ButtonPen.Location = New System.Drawing.Point(39, 71)
+        Me.ButtonPen.Name = "ButtonPen"
+        Me.ButtonPen.Size = New System.Drawing.Size(75, 55)
+        Me.ButtonPen.TabIndex = 2
+        Me.ButtonPen.UseVisualStyleBackColor = True
+        '
+        'FrmPen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.ButtonPen)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PensComboBox)
+        Me.Name = "FrmPen"
+        Me.Text = "FrmPen"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PensComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents TimerPenDrives As System.Windows.Forms.Timer
+    Friend WithEvents ButtonPen As System.Windows.Forms.Button
+
+End Class
---
+++ Leer_USB/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Public Class FrmPen
+    Private Const PenDataFolder As String = "CaptorSWin\Datos"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MostraPenDrives()
+        TimerPenDrives.Enabled = True
+
+    End Sub
+    Private Sub MostraPenDrives()
+
+        Dim allDrives() As DriveInfo = DriveInfo.GetDrives
+
+        'Compta quants dispositius removibles hi ha
+        Dim comptaPens As Integer = 0
+        For Each d In allDrives
+            If d.IsReady And d.DriveType = DriveType.Removable Then
+                comptaPens += 1
+            End If
+        Next
+
+        'Si s'ha desconnectat algun pen drive, els esborra tots
+        If comptaPens < PensComboBox.Items.Count Then
+            PensComboBox.Items.Clear()
+        End If
+
+        'Afegeix els pen drives recentment connectats
+        For Each d In allDrives
+            If d.IsReady And d.DriveType = DriveType.Removable Then
+                Dim drivestring As String = String.Format("{0}({1})", d.VolumeLabel, d.Name)
+                If Not PensComboBox.Items.Contains(drivestring) Then PensComboBox.Items.Add(drivestring)
+            End If
+        Next
+
+        If PensComboBox.Items.Count > 0 Then
+            If Not PensComboBox.Items.Contains(PensComboBox.Text) Then PensComboBox.SelectedIndex = 0
+            exitButton.Enabled = True
+            exitButton.Focus()
+        Else
+            PensComboBox.Text = String.Empty
+            exitButton.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub TimerPenDrives_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TimerPenDrives.Tick
+        MostraPenDrives()
+    End Sub
+
+    Private Sub ExitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitButton.Click
+        End
+    End Sub
+End Class
